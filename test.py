a3aaa069fdabe89c43762e75b41c8d9a20287834
paulx <xzycxy@126.com>
2017-04-25 14:08:40 +0800

    update test

--- test.py
+++ test.py
@@ -11,10 +11,8 @@
 @desc: test suite
 
 '''
-import random
 import unittest
 
-from elizabeth import Personal
 from flask_testing import TestCase
 
 from server import db, User, create_app
@@ -34,20 +32,22 @@ class testCreation(TestCase):
 
 class testUserCRUDTest(testCreation):
     def get_user(self):
-        person = Personal('en')
-        user = User(person.full_name(), str(random.randint(0, 99)), person.email())
+        user = User("test_account", "test_password", "test@test.com")
         return user
 
     def test_user_add(self):
-        self.user = self.get_user()
-        db.session.add(self.user)
+        user = self.get_user()
+        db.session.add(user)
         db.session.commit()
-        assert self.user in db.session
+        assert user in db.session
 
-    # def test_user_delete(self):
-    #     db.session.remove(self.user)
-    #     db.session.commit()
-    #     assert self.user not in db.session
+    def test_user_delete(self):
+        user = self.get_user()
+        db.session.add(user)
+        db.session.remove()
+        db.session.commit()
+        assert user not in db.session
+
 
 if __name__ == '__main__':
     unittest.main()
