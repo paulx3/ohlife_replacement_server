60b01eb30a6a5cd9642436a5349818a5ec1a2777
paulx <xzycxy@126.com>
2017-04-26 15:26:07 +0800

    update comment update

--- test.py
+++ test.py
@@ -19,29 +19,59 @@ from server.server import db, User, create_app
 
 
 class testCreation(TestCase):
+    """
+    测试flask创建
+    """
+
     def create_app(self):
+        """
+        创建app
+        Returns: flask app对象
+
+        """
         return create_app("sqlite:///")
 
     def setUp(self):
+        """
+        创建数据库
+        """
         db.create_all()
 
     def tearDown(self):
+        """
+        删除测试数据
+        """
         db.session.remove()
         db.drop_all()
 
 
 class testUserCRUDTest(testCreation):
+    """
+    测试user表的增删改查
+    """
+
     def get_user(self):
+        """
+        生成用户测试数据
+        Returns: 用户对象
+
+        """
         user = User("test_account", "test_password", "test@test.com")
         return user
 
     def test_user_add(self):
+        """
+        测试添加用户
+        """
         user = self.get_user()
         db.session.add(user)
         db.session.commit()
         assert user in db.session
 
     def test_user_delete(self):
+        """
+        测试删除用户
+        """
         user = self.get_user()
         db.session.add(user)
         db.session.remove()
